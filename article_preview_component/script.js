8b49601d9cff92c52bb78a2f17a2704e3f593da5
lakshmanan1205 <lakshmananmass143@gmail.com>
2025-02-12 15:04:35 +0530

    implement preview article improvements

--- article_preview_component/script.js
+++ article_preview_component/script.js
@@ -2,16 +2,39 @@ const ele_btn = document.getElementById("btn");
 const ele_popup = document.getElementById("popup");
 const ele_author_info = document.getElementById("author_details");
 const ele_author = document.getElementById("author");
-
+const relatedTarget = "popup";
 let is_btn_active = false;
 let lastWidth = window.innerWidth;
 
+function closePopUp() {
+  ele_popup.classList.toggle("hidden", true);
+}
+function openPopUp() {
+  ele_popup.classList.toggle("hidden", false);
+}
+function toggleActive() {
+  ele_author.classList.toggle("active");
+}
+function handleResponsive() {
+  let width = window.innerWidth;
+  lastWidth = width;
+}
+
+function fadeIn() {
+  openPopUp();
+}
+function fadeOut(e) {
+  if (e.relatedTarget.id === relatedTarget) {
+    return;
+  }
+  closePopUp();
+}
 function btnClick(e) {
   if (e.target.closest("#popup")) {
     return;
   }
-  ele_popup.className = is_btn_active ? "hidden" : "show";
-  ele_author.classList.toggle("active");
+  ele_popup.classList.toggle("hidden", is_btn_active);
+  toggleActive();
   if (lastWidth < 768) {
     ele_author_info.className = is_btn_active ? "show" : "hidden";
   }
@@ -19,8 +42,8 @@ function btnClick(e) {
 }
 function btnOut(e) {
   if (!e.target.closest("#btn") && is_btn_active) {
-    ele_popup.className = "hidden";
-    ele_author.classList.toggle("active");
+    closePopUp();
+    toggleActive();
 
     if (lastWidth < 768) {
       ele_author_info.className = is_btn_active ? "show" : "hidden";
@@ -28,11 +51,12 @@ function btnOut(e) {
     is_btn_active = !is_btn_active;
   }
 }
-function handleResponsive() {
-  let width = window.innerWidth;
-  lastWidth = width;
-}
 
 document.addEventListener("click", btnOut);
 ele_btn.addEventListener("click", btnClick);
+
+ele_btn.addEventListener("mouseenter", fadeIn);
+ele_btn.addEventListener("mouseleave", fadeOut);
+ele_popup.addEventListener("mouseleave", closePopUp);
+
 window.addEventListener("resize", handleResponsive);
